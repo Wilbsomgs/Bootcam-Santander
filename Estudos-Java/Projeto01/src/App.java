9e02226af1fa91a5fad625659d928ed7d1adf9ae
Wilbsom <willsuporte12@gmail.com>
2026-06-24 12:34:16 -0300

    adiciona projeto01

---
+++ Estudos-Java/Projeto01/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Configurando a JDK " + args[0]);
+        System.out.println("Progamando em " + args[1]);
+    }
+}
